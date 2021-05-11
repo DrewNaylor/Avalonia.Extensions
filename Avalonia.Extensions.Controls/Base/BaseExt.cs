5eb127d550b0038213e98624a41c3791f6fae51c
Ding <michael19950415@outlook.com>
2021-05-11 13:25:58 +0800

    update

---
+++ Avalonia.Extensions.Controls/Base/BaseExt.cs
@@ -0,0 +1,80 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Reflection;
+
+namespace Avalonia.Extensions.Controls
+{
+    public static class BaseExt
+    {
+        public static T GetPrivateField<T>(this Control control, string fieldName)
+        {
+            try
+            {
+                var type = control.GetType();
+                BindingFlags flag = BindingFlags.Instance | BindingFlags.NonPublic;
+                FieldInfo field = type.GetField(fieldName, flag);
+                return (T)field?.GetValue(control);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static void SetPrivateField(this Control control, string fieldName, object fieldValue)
+        {
+            try
+            {
+                var type = control.GetType();
+                BindingFlags flag = BindingFlags.Instance | BindingFlags.NonPublic;
+                var field = type.GetField(fieldName, flag);
+                field?.SetValue(control, fieldValue);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static T GetPrivateProperty<T>(this Control control, string propertyName)
+        {
+            try
+            {
+                var type = control.GetType();
+                BindingFlags flag = BindingFlags.Instance | BindingFlags.NonPublic;
+                var property = type.GetProperty(propertyName, flag);
+                return (T)property?.GetValue(control);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static void SetPrivateProperty(this Control control, string propertyName, object propertyValue)
+        {
+            try
+            {
+                var type = control.GetType();
+                BindingFlags flag = BindingFlags.Instance | BindingFlags.NonPublic;
+                var property = type.GetProperty(propertyName, flag);
+                property?.SetValue(control, propertyValue);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static object InvokePrivateMethod(this Control control, string methodName, object[] parameters = null)
+        {
+            try
+            {
+                var type = control.GetType();
+                MethodInfo methodInfo = type.GetMethod(methodName, BindingFlags.NonPublic
+                    | BindingFlags.Instance);
+                return methodInfo?.Invoke(control, parameters);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
