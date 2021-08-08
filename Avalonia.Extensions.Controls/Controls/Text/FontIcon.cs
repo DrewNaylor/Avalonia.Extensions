f7ea02189dc1be3c8c16e931976037e2505fb992
Ding <michael19950415@outlook.com>
2021-08-09 00:08:23 +0800

    fonticon in textblock

---
+++ Avalonia.Extensions.Controls/Controls/Text/FontIcon.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Extensions.Styles;
+using Avalonia.Styling;
+using System;
+
+namespace Avalonia.Extensions.Controls
+{
+    /// <summary>
+    /// https://github.com/AvaloniaUI/Avalonia/tree/feature/icons/src
+    /// </summary>
+    public class FontIcon : TemplatedControl, IStyling
+    {
+        public static readonly StyledProperty<string> GlyphProperty =
+             AvaloniaProperty.Register<FontIcon, string>(nameof(Glyph));
+        public string Glyph
+        {
+            get => GetValue(GlyphProperty);
+            set => SetValue(GlyphProperty, value);
+        }
+        Type IStyleable.StyleKey => typeof(TextBlock);
+        public FontIcon()
+        {
+            this.InitStyle();
+        }
+    }
+}
---
+++ Avalonia.Extensions.Controls/Controls/Text/FontIconSource.cs
@@ -0,0 +1,86 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+using Avalonia.Media;
+
+namespace Avalonia.Extensions.Controls
+{
+    public class FontIconSource : AvaloniaObject
+    {
+        public static readonly StyledProperty<string> GlyphProperty =
+            FontIcon.GlyphProperty.AddOwner<FontIconSource>();
+        /// <summary>
+        /// Defines the <see cref="FontFamily"/> property.
+        /// </summary>
+        public static readonly StyledProperty<FontFamily> FontFamilyProperty =
+            TextBlock.FontFamilyProperty.AddOwner<FontIconSource>();
+        /// <summary>
+        /// Defines the <see cref="FontSize"/> property.
+        /// </summary>
+        public static readonly StyledProperty<double> FontSizeProperty =
+            TextBlock.FontSizeProperty.AddOwner<FontIconSource>();
+        /// <summary>
+        /// Defines the <see cref="FontStyle"/> property.
+        /// </summary>
+        public static readonly StyledProperty<FontStyle> FontStyleProperty =
+            TextBlock.FontStyleProperty.AddOwner<FontIconSource>();
+        /// <summary>
+        /// Defines the <see cref="FontWeight"/> property.
+        /// </summary>
+        public static readonly StyledProperty<FontWeight> FontWeightProperty =
+            TextBlock.FontWeightProperty.AddOwner<FontIconSource>();
+        public string Glyph
+        {
+            get => GetValue(GlyphProperty);
+            set => SetValue(GlyphProperty, value);
+        }
+        /// <summary>
+        /// Gets or sets the font family used to draw the control's text.
+        /// </summary>
+        public FontFamily FontFamily
+        {
+            get => GetValue(FontFamilyProperty);
+            set => SetValue(FontFamilyProperty, value);
+        }
+        /// <summary>
+        /// Gets or sets the size of the control's text in points.
+        /// </summary>
+        public double FontSize
+        {
+            get => GetValue(FontSizeProperty);
+            set => SetValue(FontSizeProperty, value);
+        }
+        /// <summary>
+        /// Gets or sets the font style used to draw the control's text.
+        /// </summary>
+        public FontStyle FontStyle
+        {
+            get => GetValue(FontStyleProperty);
+            set => SetValue(FontStyleProperty, value);
+        }
+        /// <summary>
+        /// Gets or sets the font weight used to draw the control's text.
+        /// </summary>
+        public FontWeight FontWeight
+        {
+            get => GetValue(FontWeightProperty);
+            set => SetValue(FontWeightProperty, value);
+        }
+        public static StyledProperty<IBrush?> ForegroundProperty =
+            TemplatedControl.ForegroundProperty.AddOwner<FontIconSource>();
+        public IBrush? Foreground
+        {
+            get => GetValue(ForegroundProperty);
+            set => SetValue(ForegroundProperty, value);
+        }
+        public IDataTemplate IconElementTemplate { get; } = new FuncDataTemplate<FontIconSource>((source, _) => new FontIcon
+        {
+            [!ForegroundProperty] = source[!ForegroundProperty],
+            [!GlyphProperty] = source[!GlyphProperty],
+            [!FontFamilyProperty] = source[!FontFamilyProperty],
+            [!FontSizeProperty] = source[!FontSizeProperty],
+            [!FontStyleProperty] = source[!FontStyleProperty],
+            [!FontWeightProperty] = source[!FontWeightProperty]
+        });
+    }
+}
