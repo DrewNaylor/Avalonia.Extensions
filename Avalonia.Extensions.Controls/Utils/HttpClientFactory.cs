c8dc6af04f5b21ac36fe1e0e0888f8b76f253b09
Ding <michael19950415@outlook.com>
2021-08-26 16:42:41 +0800

    client

---
+++ Avalonia.Extensions.Controls/Utils/HttpClientFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+
+namespace Avalonia.Extensions
+{
+    public class HttpClientFactory
+    {
+        private static HttpClientFactory instance;
+        public static HttpClientFactory Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new HttpClientFactory();
+                return instance;
+            }
+        }
+        private IHttpClientFactory ClientFactory { get; }
+        private HttpClientFactory()
+        {
+            var serviceProvider = new ServiceCollection().AddHttpClient().BuildServiceProvider();
+            ClientFactory = serviceProvider.GetService<IHttpClientFactory>();
+        }
+        public HttpClient CreateClient()
+        {
+            return ClientFactory.CreateClient();
+        }
+    }
+}
